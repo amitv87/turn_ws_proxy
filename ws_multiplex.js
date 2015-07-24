f1d5b0f1da5e013e769683badbdfd7cf7d01c488
amit verma <amitverma@browserstack.com>
2015-07-24 20:41:21 +0530

    close turn connections on ws close

--- ws_multiplex.js
+++ ws_multiplex.js
@@ -2,12 +2,10 @@ var net = require('net'), wss = new (require('ws').Server)({ port: 8087});
 wss.on('connection', function connection(ws) {
 	console.log("connection received from proxy");
 	var conns = {}, conn_id = 0;
-
   ws.on('message', function incoming(data) {
   	if(typeof(data) == 'string'){
 			conn_id = data;
 			if(!conns[conn_id]) {
-				// console.log('recv ws createClient', data);
 				conns[conn_id] = new createClient(ws, conn_id, function(){
 					delete conns[conn_id];
 				});
@@ -16,6 +14,18 @@ wss.on('connection', function connection(ws) {
 		else if(conns[conn_id])
 			conns[conn_id].send(data);
   });
+  var killConns = function(){
+    for(var key in conns){
+      try {
+        conns[key].destroy();
+      } catch(e){}
+      delete conns[key]
+    }
+    conns = null;
+  };
+  ws.on('close', killConns);
+  ws.on('end', killConns);
+  ws.on('disconnect', killConns);
 });
 
 var createClient = function (ws, id, onClose){
@@ -30,7 +40,6 @@ var createClient = function (ws, id, onClose){
 	  else
 	    buff = buffer;
 	};
-
 	var client = new net.Socket();
 	client.connect(443, 'turn-euw2-ec2.browserstack.com', function() {
 		console.log('connected to turn', client.remotePort, client.remoteAddress);
@@ -39,15 +48,12 @@ var createClient = function (ws, id, onClose){
 			buff = null
 		}
 	});
-
 	client.on('data', function(data) {
-		// console.log('recv client ws sent', id, data);
 		if(ws && ws.readyState == 1){
 			ws.send(id);
 			ws.send(data);
 		}
 	});
-
 	client.on('close', function() {
 		console.log('connection to turn closed');
 		onClose();
@@ -57,7 +63,6 @@ var createClient = function (ws, id, onClose){
   	onClose();
   });
   this.send = function(data){
-  	// console.log('recv ws client sent', id, data);
   	if(client && client.writable)
 			client.write(data);
 		else
