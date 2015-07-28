d35921a4134e9f4ef0d0496cd65fa912cf57ed79
amit verma <amitverma@browserstack.com>
2015-07-28 15:30:29 +0530

    new buffering for tcp

--- ws_multiplex.js
+++ ws_multiplex.js
@@ -1,12 +1,13 @@
+var log = console.log;
 var net = require('net'), wss = new (require('ws').Server)({ port: 8087});
 wss.on('connection', function connection(ws) {
-	console.log("connection received from proxy");
+	log("connection received from proxy");
 	var conns = {}, conn_id = 0;
   ws.on('message', function incoming(data) {
   	if(typeof(data) == 'string'){
 			conn_id = data;
 			if(!conns[conn_id]) {
-				conns[conn_id] = new createClient(ws, conn_id, function(){
+				conns[conn_id] = new tcpClient(ws, conn_id, function(){
 					delete conns[conn_id];
 				});
 			}
@@ -28,44 +29,47 @@ wss.on('connection', function connection(ws) {
   ws.on('disconnect', killConns);
 });
 
-var createClient = function (ws, id, onClose){
-	var buff = null;
-	var storeBuff = function(buffer) {
-	  if(buff){
-	    var tmp = new Uint8Array(buff.byteLength + buffer.byteLength);
-	    tmp.set(new Uint8Array(buff), 0);
-	    tmp.set(new Uint8Array(buffer), buff.byteLength);
-	    buff = tmp.buffer;
-	  }
-	  else
-	    buff = buffer;
-	};
+var tcpClient = function (ws, conn_id, onClose){
+	var buff = [];
 	var client = new net.Socket();
 	client.connect(443, 'turn-euw2-ec2.browserstack.com', function() {
-		console.log('connected to turn', client.remotePort, client.remoteAddress);
-		if(buff && client && client.writable){
-			client.write(buff);
-			buff = null
+		log('connected to turn', client.remotePort, client.remoteAddress);
+		while (buff.length > 0 && client && client.writable){
+			tcp_send(client, buff.shift());
 		}
+		buff = [];
 	});
 	client.on('data', function(data) {
-		if(ws && ws.readyState == 1){
-			ws.send(id);
-			ws.send(data);
-		}
+		ws_send(ws, conn_id);
+		ws_send(ws, data);
 	});
 	client.on('close', function() {
-		console.log('connection to turn closed');
+		log('connection to turn closed');
 		onClose();
 	});
 	client.on('error', function (error) {
-  	console.log('turn error', error);
+		log('turn error', error);
   	onClose();
   });
   this.send = function(data){
   	if(client && client.writable)
-			client.write(data);
+			tcp_send(client, data);
 		else
-			storeBuff(data);
+			buff.push(data);
   }
+}
+
+function ws_send(ws, data){
+	try{
+		if(ws && ws.readyState == 1)
+			ws.send(data);
+	}
+	catch(e){}
+}
+
+function tcp_send(client, data){
+	try{
+		client.write(data);
+	}
+	catch(e){}
 }
